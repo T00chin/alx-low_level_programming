525cae678044da5c11df94f81b23cd4b950e6f04
T00chin <brianshawn56@gmail.com>
2023-03-16 16:57:49 -0700

    56-bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,14 @@
  */
 int main(void)
 {
-	int i;
-	float f;
-	double d;
 	char a;
-	char str[] = "Hello, world!";
+	int i;
 	long int c;
+	long long int d;
+	float f;
 printf("The size of an integer is %lu bytes.\n", sizeof(i));
 printf("The size of a float is %lu bytes.\n", sizeof(f));
-printf("The size of a double is %lu bytes.\n", sizeof(d));
-printf("The size of the string \"%s\" is %lu bytes.\n", str, sizeof(str));
+printf("The size of a long long int is %lu bytes.\n", sizeof(d));
 printf("The size of a char is %lu bytes.\n", sizeof(a));
 printf("The size of a long integer is %lu bytes.\n", sizeof(c));
 return (0);
