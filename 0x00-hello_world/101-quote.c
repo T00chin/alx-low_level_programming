72ec790a624fe77d4da202dc6752c263efb58091
T00chin <brianshawn56@gmail.com>
2023-03-16 18:02:43 -0700

    f**k

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
