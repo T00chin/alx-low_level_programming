af7e5761cb2cf8018008dc624890551d023985f9
T00chin <brianshawn56@gmail.com>
2023-03-16 17:37:00 -0700

    UNIX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A C program that prints a line to the standard error.
  * Return: 0 (Success)
  */
-int main ()
+int main(void)
 {
-	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
