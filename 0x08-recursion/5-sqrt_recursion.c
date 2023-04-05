43052e52645bde0c79b53f63cec7e1deb503c3b6
T00chin <brianshawn56@gmail.com>
2023-04-05 02:08:52 -0700

    SQUAREROOT

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,25 +16,10 @@ int _sqrt_recursion(int n)
 	{
 		return (n);
 	}
-	else
-	{
-		return (sqrt_helper(n, 1));
-	}
-}
-
-/**
- * sqrt_helper - a helper function for _sqrt_recursion
- * @n: The number to be square rooted
- * @i: The current guess of the square root
- *
- * Return: natural squareroot or -1 if doesn't exist
- */
-int sqrt_helper(int n, int i)
-{
-	if (i * i == n)
+	else if (i * i == n)
 		return (i);
 	else if (i * i > n)
 		return (-1);
 	else
-		return (sqrt_helper(n, i + 1));
+		return (_sqrt_recursion(n, i + 1));
 }
