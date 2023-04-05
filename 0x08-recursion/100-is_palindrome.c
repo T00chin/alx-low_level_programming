e238ee5748f59131486dc111ef9a09570cc07bc0
T00chin <brianshawn56@gmail.com>
2023-04-05 04:32:17 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -13,7 +13,7 @@ int _strlen_recursion(char *s);
 int is_palindrome(char *s)
 {
 	if (*s == 0)
-		return 1;
+		return (1);
 	return (check_pal(s, 0, _strlen_recursion(s)));
 }
 
@@ -26,7 +26,7 @@ int is_palindrome(char *s)
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 	return (1 + _strlen_recursion(s + 1));
 
 }
@@ -42,8 +42,8 @@ int _strlen_recursion(char *s)
 int check_pal(char *s, int i, int len)
 {
 	if (*(s + i) != *(s + len - 1))
-		return 0;
+		return (0);
 	if (i >= len)
-		return 1;
+		return (1);
 	return (check_pal(s, i + 1, len - 1));
 }
