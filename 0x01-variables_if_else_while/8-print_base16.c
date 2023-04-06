c8e76dc77a637decc9e52a5e0a66bc06a8797869
Brian Toochin <125519297+T00chin@users.noreply.github.com>
2023-04-06 08:55:12 +0300

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int n;
 	
+	
 	for (n = 0; n < 16; n++)
 	{
 		if (n < 10)
 			putchar(n + '0');
 		else if (n >= 10)
-			putchar(n -10 + 'a');
+			putchar(n - 10 + 'a');
 	}
 	putchar('\n');
 	return (0);
