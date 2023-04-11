c311ec80e24a7ac45f90b4ec229d076986196337
T00chin <brianshawn56@gmail.com>
2023-04-11 02:48:01 -0700

    Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,18 +9,19 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	char *ar;
+	unsigned int i;
+
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	char *ar;
 
 	ar = (char *) malloc(size * sizeof(char));
 	if (ar == NULL)
 	{
 		return (NULL);
 	}
-	unsigned int i;
 
 	for (i = 0; i < size; i++)
 	{
