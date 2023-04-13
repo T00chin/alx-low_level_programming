4c1d705f4ef0a9f6f8d8afc06fd088d322f4e97d
T00chin <brianshawn56@gmail.com>
2023-04-13 11:58:32 -0700

    array memory allocation

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 	ptr = malloc(nmemb * size);
-	if(ptr != NULL)
+	if (ptr != NULL)
 	{
 		memset(ptr, 0, nmemb * size);
 	}
