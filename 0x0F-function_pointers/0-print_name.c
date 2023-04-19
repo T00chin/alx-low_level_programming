df9455fb35fc29619dff9d26354f4bb9fb59fe51
T00chin <brianshawn56@gmail.com>
2023-04-19 15:28:03 -0700

    Print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -13,14 +13,24 @@ void print_name(char *name, void (*f)(char *))
 	(*f)(name);
 }
 
+/**
+ * string - print string
+ * @str: string containing name
+ */
 void string(char *str)
 {
 	printf("%s\n", str);
 }
 
-int main()
+/**
+ * main - call to print name
+ *
+ * Return: 0.
+ */
+int main(void)
 {
 	char name[] = "Brian Toochin";
+
 	print_name(name, string);
 	return (0);
 }
