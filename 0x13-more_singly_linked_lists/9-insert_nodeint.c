5d6a4930d3fc26ecbdb6c9d232c2dbe853cdceec
T00chin <brianshawn56@gmail.com>
2023-04-30 18:30:58 -0700

    insert_node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,6 +30,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
+		newNode->next = *head;
+		*head = newNode;
+	}
+	else
+	{
 		newNode->next = current->next;
 		current->next = newNode;
 	}
